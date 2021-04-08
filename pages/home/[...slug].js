ad87772ad4bd85cea461628f4414e9c9c82def18
ikasuu <71949537+ikasuu@users.noreply.github.com>
2021-04-07 23:40:09 -0700

    Fixed getStaticPaths in url redirect

--- pages/home/[...slug].js
+++ pages/home/[...slug].js
@@ -5,13 +5,13 @@ function OldSectionPage() {
         <div>
             This page is unused, it is for redirecting users from previous URL to new site URL structure
         </div>
-    )
+    );
 }
 
 // This function gets called at build time
 export async function getStaticPaths() {
 
-    return { paths: [{ params: { slug: 'content' } }, { params: { id: 'classes' }, }, { params: { id: 'events' }, }, { params: { id: 'resources' }, }], fallback: false }
+    return { paths: [{params: { slug: ['content'] }}, {params: { slug: ['classes'] }}, {params: { slug: ['events'] }}, {params: { slug: ['resources'] }}], fallback: false }
   }
   
 // This also gets called at build time
@@ -25,4 +25,4 @@ export async function getStaticProps({ params }) {
     }
 }
 
-export default OldSectionPage();
+export default OldSectionPage;
