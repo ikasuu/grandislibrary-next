930f63b7b1c53dbc85dcadbef263d98e27ac1bd4
ikasuu <71949537+ikasuu@users.noreply.github.com>
2022-04-07 20:11:49 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,45 +1,44 @@
-// import Document from 'next/document'
-// import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+// import Script from 'next/script';
+import { ServerStyleSheet } from 'styled-components';
 
-// export default class MyDocument extends Document {
-//   static async getInitialProps(ctx) {
-//     const sheet = new ServerStyleSheet()
-//     const originalRenderPage = ctx.renderPage
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-//     try {
-//       ctx.renderPage = () =>
-//         originalRenderPage({
-//           enhanceApp: (App) => (props) =>
-//             sheet.collectStyles(<App {...props}/>),
-//         })
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props}/>),
+        })
 
-//       const initialProps = await Document.getInitialProps(ctx)
-//       return {
-//         ...initialProps,
-//         styles: (
-//           <>
-//             {initialProps.styles}
-//             {sheet.getStyleElement()}
-//           </>
-//         ),
-//       }
-//     } finally {
-//       sheet.seal()
-//     }
-//   }
-// }
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 
-import { Html, Head, Main, NextScript } from 'next/document'
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2213677142380749" crossOrigin="anonymous"></script> */}
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  render() {
+    return(
+      <Html lang="en">
+        {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2213677142380749" crossOrigin="anonymous"/> */}
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
