208e80d42c3b015b204d730345a410f1c05fef44
ikasuu <71949537+ikasuu@users.noreply.github.com>
2022-04-14 20:55:37 -0700

    Enable ads + using Next js 10

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Suspense } from 'react';
 import { ThemeProvider } from 'styled-components';
 import storage from 'local-storage-fallback';
 import Head from 'next/head';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 import { getInitialTheme, GlobalStyle } from '../special/GlobalTheme';
 import MainNavbar from '../components/MainNavbar';
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }) {
         <title>Grandis Library</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <meta name="description" property="og:description" content="A collection of MapleStory guides, resources and information." key="desc"/>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2213677142380749" crossOrigin="anonymous"/>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2213677142380749" crossOrigin="anonymous"/>
       </Head>
       <ThemeProvider theme={theme}>
         <>
