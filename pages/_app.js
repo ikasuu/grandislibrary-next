b0b04e93fefd7933b42ffee866048c321d92e675
Ikasuu <71949537+ikasuu@users.noreply.github.com>
2022-10-03 12:36:13 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
         <title>Grandis Library</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_ID}`} crossOrigin="anonymous"/>
-        <script src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GA_ID}`}/>
+        {/* <script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}/>
         <script>
           {`
             window.dataLayer = window.dataLayer || [];
@@ -42,7 +42,7 @@ function MyApp({ Component, pageProps }) {
 
             gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
           `}
-        </script>
+        </script> */}
       </Head>
       <ThemeProvider theme={theme}>
         <>
