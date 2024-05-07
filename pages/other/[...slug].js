862798d421d18d9da2f3b028828aa9414f04b9bb
Ikasuu <71949537+ikasuu@users.noreply.github.com>
2024-05-06 21:16:22 -0700

    Added Lynn promo image

--- pages/other/[...slug].js
+++ pages/other/[...slug].js
@@ -31,7 +31,7 @@ function ClassOverview({post, slug}) {
             </Head>
             <HeaderImage imageUrl={HeaderImageUrl.library}/>
             <Container>
-              {BTFlag && <Alert variant="dark">As of April 2024, Beast Tamer has been replaced with the new class Lynn. For more info, please read Nexon's offical <a href="https://maplestory.nexon.net/news/88931/maple-memo-gms-roadmap-q1-2024" target="_blank" rel="noreferrer noopener">Maple Memo</a></Alert>}
+              {BTFlag && <Alert variant="dark">As of May 2024, Beast Tamer has been replaced with the new class Lynn. For more info, please read Nexon's offical <a href="https://maplestory.nexon.net/news/88931/maple-memo-gms-roadmap-q1-2024" target="_blank" rel="noreferrer noopener">Maple Memo</a></Alert>}
               <ClassIntro data={data}/>
               {data.content.howToCreate && <ClassCreation classTitle={data.class} howToCreate={data.content.howToCreate}/>}
               {data.content.extraContent && <ClassExtraContent title={data.content.extraContent.title} content={data.content.extraContent.content}/>}
