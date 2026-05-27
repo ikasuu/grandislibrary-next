10d77cd6eddc6e8da484fc89d5ecf65a13150953
Ikasuu <71949537+ikasuu@users.noreply.github.com>
2026-05-26 22:41:00 -0700

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -42,17 +42,15 @@ function Privacy() {
             <HeaderImage imageUrl={HeaderImageUrl.library}/>
             <Container>
                 <PrivacyTitle>Privacy Policy</PrivacyTitle>
-                <PrivacySubtitle>Last Revised October 19th, 2022</PrivacySubtitle>
+                <PrivacySubtitle>Last Revised May 26th, 2026</PrivacySubtitle>
                 <p>This page informs you of how Grandis Library ("us", "we", or "our") collects, uses, and shares information about you when you use our site grandislibrary.com.</p>
                 <p>By using our site, you agree to the collection and use of information in accordance with this policy.</p> 
                 <PrivacySubject>Advertising and Analytics</PrivacySubject>
-                <p>We use third party advertisements to support our site. Some of these advertisers may be served from our third party advertiser. You can view their privacy policy and cookie policy here. You can manage your cookies here at <a class="nn-cmp-show" href="#">Manage Cookie Settings</a>.</p>
-                <p>We use Google Analytics, Cloudflare Analytics, and Comscore to analyse the use of our website. Google Analytics and Comscore gather information about website use by means of cookies however, Cloudflare Analytics do not use cookies. The information gathered relating to our website is used to create reports about the use of our website.</p>
+                <p>We use Google Analytics and Cloudflare Analytics to analyse the use of our website. Google Analytics gathers information about website use by means of cookies however, Cloudflare Analytics do not use cookies. The information gathered relating to our website is used to create reports about the use of our website.</p>
                 <p>Google's privacy policy is available at: <a href="https://www.google.com/policies/privacy/" target="_blank" rel="noreferrer noopener">https://www.google.com/policies/privacy/</a>.</p>
-                <p>Comscore’s privacy policy is available at <a href="https://www.comscore.com/About-comScore/Privacy-Policy" target="_blank" rel="noreferrer noopener">https://www.comscore.com/About-comScore/Privacy-Policy</a>.</p>
                 <PrivacySubject>Cookies</PrivacySubject>
                 <p>Grandis Library does not directly use cookies however third party advertisers and analytics use cookies for services such as serving and rendering ads, and user statistics. By accessing Grandis Library, you consent to the collection and use of your information by these third party vendors.</p>
-                <p>Google Analytics and Comscore uses cookies however Cloudflare Analytics does not use any cookies. Google Analytics uses cookies to collect information and report site usage statistics without personally identifying individual users.</p>
+                <p>Google Analytics uses cookies however Cloudflare Analytics does not use any cookies. Google Analytics uses cookies to collect information and report site usage statistics without personally identifying individual users.</p>
                 <p>To learn more about how Google Analytics uses cookies, please visit <a href="https://policies.google.com/technologies/cookies" target="_blank" rel="noopener noreferrer">https://policies.google.com/technologies/cookies</a></p>
                 <p>To learn more about Cloudflare Analytics, please visit <a href="https://www.cloudflare.com/en-ca/web-analytics/" target="_blank" rel="noopener noreferrer">https://www.cloudflare.com/en-ca/web-analytics/</a></p>
                 <PrivacySubject>Local Storage</PrivacySubject>
